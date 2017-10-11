61fa21207d78028d44f5281307584a49d647c4be
Michael Hansen <michael.hansen@microsoft.com>
2017-10-11 13:07:29 +0000

    Fixed bugs in NS setup script

--- PrivateTrafficManager/scripts/setup_ns.sh
+++ PrivateTrafficManager/scripts/setup_ns.sh
@@ -2,13 +2,17 @@
 
 sql_server_password=$1
 
-debconf-set-selections <<< "mysql-server mysql-server/root_password $sql_server_password password"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again $sql_server_password verify_password"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $sql_server_password"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $sql_server_password"
 
-debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/password-confirm $sql_server_password password"
-debconf-set-selections <<< "pdns-backend-mysql  pdns-backend-mysql/app-password-confirm $sql_server_password password"
-debconf-set-selections <<< "pdns-backend-mysql  pdns-backend-mysql/mysql/app-pass $sql_server_password password"
-debconf-set-selections <<< "pdns-backend-mysql   pdns-backend-mysql/mysql/admin-pass $sql_server_password password"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/password-confirm password $sql_server_password"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/app-password-confirm password $sql_server_password"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/mysql/app-pass password $sql_server_password"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/mysql/admin-pass password $sql_server_password"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/dbconfig-remove select true"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/dbconfig-reinstall select false"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/dbconfig-upgrade select true"
+debconf-set-selections <<< "pdns-backend-mysql pdns-backend-mysql/dbconfig-install select true"
 
 apt-get update 
 apt-get -y install mysql-server mysql-client  pdns-server pdns-backend-mysql pdns-server pdns-backend-remote
